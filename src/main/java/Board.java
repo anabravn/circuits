e08ea00c64bb27c85264f1477f1257a7e8ffbc83
anabravn <73392803+anabravn@users.noreply.github.com>
2024-12-10 19:41:25 -0300

    Refatorar tabuleiro para utilizar array unidimensional

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,7 +1,6 @@
-import java.util.ArrayList;
 
 public class Board {
-    boolean[][] board;
+    boolean[] board;
     Integer width;
     Integer height;
     Integer size;
@@ -12,31 +11,38 @@ public class Board {
      * @param h Altura do tabuleiro
      */
     public Board(Integer w, Integer h) {
-        board = new boolean[h][w];
+        height = h;
+        width = w;
+        size = h * w;
 
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                board[i][j] = false;
-            }
+        board = new boolean[size];
+
+        for (int i = 0; i < size; i++) {
+            board[i] = false;
         }
 
-        width = w;
-        height = h;
-        size = w * h;
     }
 
     /**
      * Liga uma célula do tabuleiro.
      */
-    public void set(int x, int y) {
-        board[y][x] = true;
+    public void set(Integer x, Integer y) {
+        board[getIndex(x, y)] = true;
+    }
+
+    public void set(Integer index) {
+        board[index] = true;
     }
 
     /**
-     * Desliga uma célula do tabuleiro.
+     * Retorna true se a célula está ligada.
      */
     public boolean get(Integer x, Integer y) {
-        return board[y][x];
+        return board[getIndex(x, y)];
+    }
+
+    public boolean get(Integer index) {
+        return board[index];
     }
 
     /** 
@@ -48,7 +54,7 @@ public class Board {
         
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                out = out.concat(board[i][j] ? "x " : "  ");
+                out = out.concat(board[getIndex(j, i)] ? "x " : "  ");
             }
             out = out.concat("\n");
         }
@@ -77,24 +83,9 @@ public class Board {
         return node;
     }
 
-    private Integer getX(Integer id) {
-        return id % width;
+    private Integer getIndex(Integer x, Integer y) {
+        return y * width + x;
     }
-
-    private Integer getY(Integer id) {
-        return id / width;
-    }
-
-    /**
-     * Retorna true se a célula está ligada.
-     * Usado para identificar os nós válidos do grafo.
-     * @param id
-     */
-    private boolean getValue(int id) {
-        int x = getX(id), y = getY(id);
-        return get(x, y);
-    }
-    
 
     /**
      * Busca em profundidade a partir de um nó fonte
@@ -109,7 +100,7 @@ public class Board {
             parent[i] = -1;
         }
 
-        if (getValue(source) && parent[source] == -1) {
+        if (get(source) && parent[source] == -1) {
             parent = dfsVisit(source, parent);
         }
 
@@ -120,14 +111,14 @@ public class Board {
         Integer l = leftChild(node);
         Integer r = rightChild(node);
 
-        if(l != -1 && getValue(l)) {
+        if(l != -1 && get(l)) {
             if(parent[l] == -1) {
                 dfsVisit(l, parent);
             }
             parent[l] = node;
         }
 
-        if(r != -1 && getValue(r)) {
+        if(r != -1 && get(r)) {
             if(parent[r] == -1) {
                 dfsVisit(r, parent);
             }
@@ -141,7 +132,7 @@ public class Board {
 
         for (int i = 0; i < size - 1; i++ ) {
             if ((i+1) % width != 0 && 
-                getValue(i) && getValue(i+1) && 
+                get(i) && get(i+1) && 
                 parent[i] != -1 && parent[i+1] != i) {
                     return true;
                 }
